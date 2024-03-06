6407e032b7b5e6ca342889c8f0822ff30c27d1d7
mdxv <mohan.elias@proton.me>
2024-03-06 18:56:30 -0300

    Revert "feat: shadcn and tailwindcss"
    This reverts commit 31dd28e73ea0f7f31cf4c4fa4e1f2673d48875bd.

--- src/components/Handler.jsx
+++ src/components/Handler.jsx
@@ -14,7 +14,7 @@ const Handler = () => {
     // REDDIT
     case /^r$/g.test(input):
     case /^r\/+/g.test(input):
-      body.style.backgroundColor = "#ff4500";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(255, 132, 86), rgb(255, 69, 0))";
       body.style.color = "#d4d4d4";
       break;
 
@@ -22,7 +22,7 @@ const Handler = () => {
     case /^tt\/+/g.test(input):
     case /^tt$/g.test(input):
     case /^tt:/g.test(input):
-      body.style.backgroundColor = "#1a8cd8";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(29, 161, 242), rgb(25, 96, 143))";
       body.style.color = "#d4d4d4";
       break;
 
@@ -30,7 +30,7 @@ const Handler = () => {
     case /^y\/+/g.test(input):
     case /^y$/g.test(input):
     case /^y:/g.test(input):
-      body.style.backgroundColor = "#ff0000";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(205, 32, 31), rgb(205, 76, 31))";
       body.style.color = "#d4d4d4";
       break;
 
@@ -38,14 +38,14 @@ const Handler = () => {
     case /^g\/+/g.test(input):
     case /^g$/g.test(input):
     case /^g:/g.test(input):
-      body.style.backgroundColor = "#1a1e22";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(28, 33, 40), rgb(45, 51, 59)";
       body.style.color = "#d4d4d4";
       break;
 
     // INSTAGRAM
     case /^i\/+/g.test(input):
     case /^i$/g.test(input):
-      body.style.backgroundColor = "#f70275";
+      body.style.backgroundImage = "linear-gradient(45deg, rgb(64, 93, 230), rgb(88, 81, 219), rgb(131, 58, 180), rgb(193, 53, 132), rgb(225, 48, 108), rgb(253, 29, 29)";
       body.style.color = "#d4d4d4";
       break;
 
@@ -53,7 +53,7 @@ const Handler = () => {
     case /^tw\/+/g.test(input):
     case /^tw$/g.test(input):
     case /^tw:/g.test(input):
-      body.style.backgroundColor = "#8c44f7";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(169, 112, 255), rgb(67, 44, 101)";
       body.style.color = "#d4d4d4";
       break;
 
@@ -61,21 +61,21 @@ const Handler = () => {
     case /^mk\/+/g.test(input):
     case /^mk$/g.test(input):
       body.style.backgroundImage = "none";
-      body.style.backgroundcolor = "#323437";
+      body.style.backgroundColor = "#323437";
       body.style.color = "#d4d4d4";
       break;
 
     // SPOTIFY
     case /^s\/+/g.test(input):
     case /^s$/g.test(input):
-      body.style.backgroundColor = "#1ed760";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(29, 211, 94), rgb(30, 215, 96)";
       body.style.color = "#d4d4d4";
       break;
 
     // WHATSAPP
     case /^w\/+/g.test(input):
     case /^w$/g.test(input):
-      body.style.backgroundColor = "#2ab13f";
+      body.style.backgroundImage = "linear-gradient(135deg, rgb(37, 211, 102), rgb(18, 140, 126), rgb(7, 94, 84))";
       body.style.color = "#d4d4d4";
       break;
 
@@ -91,6 +91,7 @@ const Handler = () => {
     // DISCORD
     case /^dc\/+/g.test(input):
     case /^dc$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#7289da";
       body.style.color = "#d4d4d4";
       break;
@@ -98,6 +99,7 @@ const Handler = () => {
     // GMAIL
     case /^m\/+/g.test(input):
     case /^m$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#dd5145";
       body.style.color = "#d4d4d4";
       break;
@@ -105,27 +107,28 @@ const Handler = () => {
     // NETFLIX
     case /^n\/+/g.test(input):
     case /^n$/g.test(input):
-      body.style.backgroundColor = "#e50914";
+      body.style.backgroundImage = "linear-gradient(135deg, #E50914, #CB020C)";
       body.style.color = "#d4d4d4";
       break;
 
     // HACKTHEBOX 
     case /^htb\/+/g.test(input):
     case /^htb$/g.test(input):
-      body.style.backgroundColor = "#101825";
+      body.style.backgroundImage = "linear-gradient(135deg, #1A2332, #111927)";
       body.style.color = "#9FEF00";
       break;
     
     // TRYHACKME
     case /^thm\/+/g.test(input):
     case /^thm$/g.test(input):
-      body.style.backgroundColor = "#323a40";
+      body.style.backgroundImage = "linear-gradient(135deg, #1C2538, #161E2D)";
       body.style.color = "#d4d4d4";
       break;
 
 		// PRIME VIDEO
 		case /^pv\/+/g.test(input):
 		case /^pv$/g.test(input):
+      body.style.backgroundImage = "none";
 			body.style.backgroundColor = "#1b242f";
       body.style.color = "#d4d4d4";
 			break;
@@ -133,6 +136,7 @@ const Handler = () => {
     // PINTEREST
 		case /^p\/+/g.test(input):
 		case /^p$/g.test(input):
+      body.style.backgroundImage = "none";
 			body.style.backgroundColor = "#c42027";
       body.style.color = "#d4d4d4";
 			break;
@@ -140,20 +144,21 @@ const Handler = () => {
     // LINKEDIN
 		case /^l\/+/g.test(input):
 		case /^l$/g.test(input):
-			body.style.backgroundColor = "#0073b1";
+      body.style.backgroundImage = "linear-gradient(135deg, #0a66c2, #0d86ff)";
       body.style.color = "#d4d4d4";
 			break;
 
     // ALIEXPRESS
 		case /^a\/+/g.test(input):
 		case /^a$/g.test(input):
-      body.style.backgroundColor = "#df2e05";
+      body.style.backgroundImage = "linear-gradient(135deg, #E52D03, #FD9300)";
       body.style.color = "#d4d4d4";
 			break;
 
     // TRELLO
 		case /^t\/+/g.test(input):
 		case /^t$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#0077bd";
       body.style.color = "#d4d4d4";
 			break;
@@ -161,6 +166,7 @@ const Handler = () => {
     // CHATGPT
 		case /^c\/+/g.test(input):
 		case /^c$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#343541";
       body.style.color = "#bdbdca";
 			break;
@@ -168,6 +174,7 @@ const Handler = () => {
     // OUTLOOK
 		case /^o\/+/g.test(input):
 		case /^o$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#0f6cbd";
       body.style.color = "#bdbdca";
 			break;
@@ -175,6 +182,7 @@ const Handler = () => {
     // NOTION
 		case /^no\/+/g.test(input):
 		case /^no$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#ffffff";
       body.style.color = "#37352f";
 			break;
@@ -182,13 +190,14 @@ const Handler = () => {
     // GDRIVE
     case /^d\/+/g.test(input):
     case /^d$/g.test(input):
-      body.style.backgroundColor = "#2581f5";
+      body.style.backgroundImage = "linear-gradient(135deg, #FFD04B, #1EA362, #4688F3)";
       body.style.color = "#d4d4d4";
       break;
 
     // HABBO
 		case /^h\/+/g.test(input):
 		case /^h$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#f7c600";
       body.style.color = "#000000";
 			break;
@@ -196,6 +205,7 @@ const Handler = () => {
     // UNISANTOS
 		case /^u\/+/g.test(input):
 		case /^u$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#52659b";
       body.style.color = "#FFFFFF";
 			break;
@@ -203,13 +213,14 @@ const Handler = () => {
     // CANVA
 		case /^cv\/+/g.test(input):
 		case /^cv$/g.test(input):
-      body.style.backgroundColor = "#01c3cc";
+      body.style.backgroundImage = "linear-gradient(135deg, #01c3cc, #7d2ae7)";
       body.style.color = "#FFFFFF";
 			break;
 
     // PONTOGO 
 		case /^pg\/+/g.test(input):
 		case /^pg$/g.test(input):
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#000099";
       body.style.color = "#FFFFFF";
 			break;
@@ -217,11 +228,12 @@ const Handler = () => {
     // ZOHO 
 		case /^z\/+/g.test(input):
 		case /^z$/g.test(input):
-      body.style.backgroundColor = "#387bbb";
+      body.style.backgroundImage = "linear-gradient(135deg, #e42527, #089949, #226db4, #f9b21d)";
       body.style.color = "#FFFFFF";
 			break;
 
     default:
+      body.style.backgroundImage = "none";
       body.style.backgroundColor = "#191919";
       body.style.color = "#d4d4d4";
       break;
